b3eabb06a813dbe84b210a2488fac3325645ca20
skks1991 <basher.send@gmail.com>
2023-10-24 20:18:10 +0600

    updated

--- rearrange.js
+++ rearrange.js
@@ -1,7 +1,35 @@
 
 
 function r1(){
-  document.getElementById("demo").innerHTML = "g";
+  document.getElementById("demo1").innerHTML = "g";
+  
+}
+function r2(){
+  document.getElementById("demo2").innerHTML = "b";
+  
+}
+function r3(){
+  document.getElementById("demo3").innerHTML = "f";
+  
+}
+function r4(){
+  document.getElementById("demo4").innerHTML = "d";
+  
+}
+function r5(){
+  document.getElementById("demo5").innerHTML = "a";
+  
+}
+function r6(){
+  document.getElementById("demo6").innerHTML = "h";
+  
+}
+function r7(){
+  document.getElementById("demo7").innerHTML = "c";
+  
+}
+function r8(){
+  document.getElementById("demo8").innerHTML = "e";
   
 }
 
