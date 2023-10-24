5df0c6e05e01b4514af8867714b6057fb7150747
skks1991 <basher.send@gmail.com>
2023-10-24 19:46:34 +0600

    updated

--- rearrange.js
+++ rearrange.js
@@ -1,14 +1,7 @@
-const firstAns = a;
-let a = "(g) Stephen Hawking was born to an educated family.";
-if (firstAns == a) {
-  greeting = "Good morning";
-} else if (time < 20) {
-  greeting = "Good day";
-} else {
-  greeting = "Good evening";
-}
-document.getElementById("demo").innerHTML = greeting;
+
 
 function r1(){
   document.getElementById("demo").innerHTML = "g";
+  
 }
+
